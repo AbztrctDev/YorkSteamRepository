6cb46637a6854e3c11275714ddce510774c125c3
AbztrctDev <110995459+AbztrctDev@users.noreply.github.com>
2022-08-31 19:17:56 -0500

    ldadad

---
+++ Tabla Periodica 3D/Assets/list.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class list : MonoBehaviour
+{
+
+
+    public GameObject hidrogeno;
+    public GameObject litio;
+    public GameObject sodio;
+    public GameObject potasio;
+    public GameObject rubidio;
+    public GameObject cesio;
+    public GameObject francio;
+    public Text _description;
+    public Text _titulo;
+    public Text _propiedades;
+
+    public string descHidrogeno;
+
+    private List<string> _list = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
